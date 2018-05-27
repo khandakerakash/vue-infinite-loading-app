b97fd4728002a1e893afc7204194958eaf94af92
Khandaker Akash <akash.cse10@gmail.com>
2018-05-27 20:35:05 +0600

    Initial TEST Data Load with scroll

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div>
+      <p v-for="item in list">
+        Line:
+        <span v-text="item"></span>
+      </p>
+      <infinite-loading @infinite="infiniteHandler"></infinite-loading>
+    </div>
+  </div>
+</template>
+
+<script>
+  import InfiniteLoading from 'vue-infinite-loading';
+  export default {
+    name: 'app',
+    data() {
+      return {
+        list: [],
+      };
+    },
+    methods: {
+      infiniteHandler($state) {
+        setTimeout(() => {
+          const temp = [];
+          for (let i = this.list.length + 1; i <= this.list.length + 20; i++) {
+            temp.push(i);
+          }
+          this.list = this.list.concat(temp);
+          $state.loaded();
+        }, 1000);
+      },
+    },
+    components: {
+      InfiniteLoading,
+    },
+  };
+  </script>
+
+  <style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
